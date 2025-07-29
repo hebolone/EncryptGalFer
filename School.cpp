e3f472fcee44e06a3c4cc584c581c32a3b851c18
Hebolone <polo.simone@gmail.com>
2025-07-29 20:01:32 +0200

    Code refine with CLion

--- School.cpp
+++ School.cpp
@@ -10,10 +10,10 @@ int main() {
     //std::cout << "Hello, " << *author << std::endl;
     //std::cout << std::format("Hello, {}", *author) << std::endl;
 
-    std::unique_ptr<EncryptSimple> encrypter = std::make_unique<EncryptSimple>();
-    const std::string original_simple { "GFTAVFDOX" };
-    std::string converted = original_simple;
-    int times = 10;
+    // std::unique_ptr<EncryptSimple> encrypter = std::make_unique<EncryptSimple>();
+    // const std::string original_simple { "GFTAVFDOX" };
+    // std::string converted = original_simple;
+    // int times = 10;
 
     // for(int i = 0; i < times; i ++) {
     //     std::string in = converted;
--- Vigenere.hpp
+++ Vigenere.hpp
@@ -4,25 +4,24 @@
 
 class Vigenere {
 private:
-    const std::string _Alphabet { "ABCDEFGHIJKLMNOPQRSTUVWXYZ" };
-    const int _Length = _Alphabet.length();
+    const std::string Alphabet { "ABCDEFGHIJKLMNOPQRSTUVWXYZ" };
+    const int Length = Alphabet.length();
 public:
     std::string encrypt(const std::string & source, const std::string & key) const {
         std::string s { source };
-        std::transform(s.begin(), s.end(), s.begin(), [](unsigned char c) { return std::toupper(c); });
+        std::ranges::transform(s, s.begin(), [](unsigned char c) { return std::toupper(c); });
 
         std::string k { key };
-        std::transform(k.begin(), k.end(), k.begin(), [](unsigned char c) { return std::toupper(c); });
+        std::ranges::transform(k, k.begin(), [](unsigned char c) { return std::toupper(c); });
         
         std::string return_value { };
-        int length = s.length();
+        const int length = s.length();
         int key_counter = 0;
         for(int i = 0; i < length; i ++) {
-            size_t column_index = _Alphabet.find(s[i]);
-            if(column_index != std::string::npos) {
-                char key_char = k[(key_counter ++) % k.length()];
-                size_t row_index = _Alphabet.find(key_char);
-                char result = _Alphabet[(column_index + row_index) % _Length];
+            if(size_t column_index = Alphabet.find(s[i]); column_index != std::string::npos) {
+                const char key_char = k[(key_counter ++) % k.length()];
+                const size_t row_index = Alphabet.find(key_char);
+                const char result = Alphabet[(column_index + row_index) % Length];
                 return_value += result;
             } else {
                 return_value += s[i];
@@ -32,22 +31,21 @@ public:
     }
     std::string decrypt(const std::string & source, const std::string & key) const {
         std::string s { source };
-        std::transform(s.begin(), s.end(), s.begin(), [](unsigned char c) { return std::toupper(c); });
+        std::ranges::transform(s, s.begin(), [](unsigned char c) { return std::toupper(c); });
 
         std::string k { key };
-        std::transform(k.begin(), k.end(), k.begin(), [](unsigned char c) { return std::toupper(c); });
+        std::ranges::transform(k, k.begin(), [](unsigned char c) { return std::toupper(c); });
         
         std::string return_value { };
-        int length = s.length();
+        const int length = s.length();
         int key_counter = 0;
         for(int i = 0; i < length; i ++) {
             char key_char = k[key_counter % k.length()];
-            size_t row_index = _Alphabet.find(key_char);
-            size_t search_char = _Alphabet.find(s[i]);
-            if(search_char != std::string::npos) {
+            const size_t row_index = Alphabet.find(key_char);
+            if(size_t search_char = Alphabet.find(s[i]); search_char != std::string::npos) {
                 key_counter++;
-                size_t intersection = (_Length - row_index + search_char) % _Length;
-                return_value += _Alphabet[intersection];
+                const size_t intersection = (Length - row_index + search_char) % Length;
+                return_value += Alphabet[intersection];
             } else {
                 return_value += s[i];
             }
